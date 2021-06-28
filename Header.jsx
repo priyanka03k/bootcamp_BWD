8069e15b888b6a70f5b5640e884e14cedcf488e0
PRIYANKA <67974036+priyanka03k@users.noreply.github.com>
2021-06-28 21:40:18 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Project Of ShapeAI </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScrpit and React.js</h1>
+      <p>
+        A basic web dev React Js Bootcamp.This was an amazing bootcamp taken up
+        by Shaurya Sinha We covered everything from scratch including JavaScript
+        React.js,HTML. Thank You ShapeAI !!
+      </p>
+    </div>
+  );
+}
+
+export default Note;
